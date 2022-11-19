c3a9655285ee1dc27b03c5031185dabea48a3649
chaudhuree <chaudhuree00@gmail.com>
2022-11-19 21:45:51 +0600

    extra credit 2 and 3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,11 +2,25 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+//? : normal exercise
+// const countReducer = (state, action) => {
+//   return action
+// }
+//? : extra credit 01
+// const countReducer = (state, action) => {
+//   return action + state
+// }
+
+//? : extra credit 02
+// const countReducer = (state, action) => {
+//   return ({...state,...action})  //* ({...state=({count:initialCount=0}),...action=({count:count+step=>1,2,3})})
+// }
+//? ex cr: 03
 const countReducer = (state, action) => {
-  return action + state
+  return {...state, ...(typeof action === 'function' ? action(state) : action)}
 }
 function Counter({initialCount = 0, step = 1}) {
-
+  //? : normal exercise
   // const [count, setCount] = React.useReducer(countReducer,initialCount)
 
   // 💰 you can write the countReducer function so you don't have to make any
@@ -15,8 +29,25 @@ function Counter({initialCount = 0, step = 1}) {
   // The 2nd argument is called "newState" - the value passed to setCount
 
   // const increment = () => setCount(count + step)
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
+  //? : extra credit 01
+  // const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  // const increment = () => changeCount(step)
+
+  //? : extra credit 02
+  // const [state, setState] = React.useReducer(countReducer, {
+  //   count: initialCount,
+  // })
+  // const {count} = state
+  // const increment = () => setState({count: count + step})
+
+  //? ex cr: 03
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
+
   return <button onClick={increment}>{count}</button>
 }
 
